526c1019dfde81851224237ce7fb00f0066455bf
William Caldwell <williamcaldwell@MacBook-Air.local>
2017-01-24 15:35:29 -0600

    added js file

---
+++ javascript/esperanto.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// empty iife...
+
+var translateText = (function () {
+
+      var privateMethod = function () {};
+
+      return {
+        publicMethodOne: function () {
+          // I can call `privateMethod()` you know...
+        },
+        publicMethodTwo: function () {
+
+        },
+        publicMethodThree: function () {
+
+        }
+      };
+
+})();
