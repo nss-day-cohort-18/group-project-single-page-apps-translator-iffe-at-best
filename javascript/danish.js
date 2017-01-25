eb8be0202ac4f5199d33761de12c257241c132f7
Dara Thomas <darathomas@Daras-MacBook-Air.local>
2017-01-25 10:11:22 -0600

    updated danish.js

---
+++ javascript/danish.js
@@ -0,0 +1,34 @@
+"use strict";
+
+var TranslateText = (function (TranslateText){
+	var danishTranslate = {
+	    "merry": "lystig",
+		"christmas": "jul",
+		"and": "og",
+		"happy": "lykkelig",
+		"new": "ny",
+		"year": "år"
+	};
+
+	return {
+		getDanishTranslation: function(danishWord){
+			return danishTranslate[danishWord]
+		}
+	  
+	};
+	  return TranslateText;
+})(TranslateText);
+console.log(TranslateText);
+
+
+
+
+
+TranslateText.getDanishTranslation = function (){
+	for (var i = 0; i < danish.length; i++) {
+ 
+  }
+
+
+
+   
